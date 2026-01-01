97d388b248f127a1c66268e97e79f233bc33b06f
Jashuva <jashujaan98@gmail.com>
2026-01-01 18:14:23 +0530

    react login and register page

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
+
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
 
 app.use('/webhooks/stripe', require('./routes/webhook.routes'));
 app.use(express.json());
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+
+import './Home.css';
+function Home() {
+  return (
+    <div className="home-container">
+      <h2>Welcome to the Home Page</h2>
+      <p>This is the main landing page of the application.</p>
+    </div>
+  );
+}
+
+export default Home;
